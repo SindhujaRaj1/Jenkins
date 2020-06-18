648b66b89df822d1b6da16aeca4b4d5273c1b526
HP <HP@DESKTOP-OLKOA39>
2020-06-18 18:08:17 +0530

    Switch

---
+++ src/com/org/switch1/Switch.java
@@ -0,0 +1,43 @@
+package com.org.switch1;
+
+
+
+public class Switch {
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Enter number from 1 to 7:");
+		int a=3;
+		System.out.println("Enter number is 3");
+		
+		
+		switch(a){
+		case 1:
+			System.out.println("Monday");
+			break;
+		case 2:
+			System.out.println("Tuesday");
+			break;
+		case 3:
+			System.out.println("Wednesday");
+			break;
+		case 4:
+			System.out.println("Thrusday");
+			break;
+		case 5:
+			System.out.println("Friday");
+			break;
+		case 6:
+			System.out.println("Saturday");
+			break;
+		case 7:
+			System.out.println("Sunday");
+			break;
+		default:
+			System.out.println("Enter correct choice");
+			break;
+		}
+	}
+	}
+
+
